66ccdded4800c9866f1d16804d8f604de5cf1045
FarrellIon <104186473+FarrellIon@users.noreply.github.com>
2024-04-17 16:49:15 +0700

    remove express learning folder

--- Backend/src/database/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose
-    .connect(process.env.DB_URL)
-    .then(() => console.log('Connected to dB'))
-    .catch((err) => console.log(err));
--- Frontend/components/buku-baru-terbit.vue
+++ Frontend/components/buku-baru-terbit.vue
@@ -1,19 +1,15 @@
 <template>
     <div>
         <h1>Test</h1>
-        <div v-for="book in books">
+        <!-- <div v-for="book in books">
             <h1>{{books}}</h1>
-        </div>
+        </div> -->
     </div>
 </template>
 
 <script setup>
-    // const { apiToken, shopID } = useRuntimeConfig();
-    console.log(apiToken);
-    const url = `https://shopee-e-commerce-data.p.rapidapi.com/shopee/shop/items/v2?site=id&shop_id=212700275&page=1&pageSize=20`;
-    const { data: books } = await useFetch(url);
-
-    console.log(books);
+    // const url = ``;
+    // const { data: books } = await useFetch(url);
 </script>
 
 <style lang="scss" scoped>
