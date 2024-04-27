37f56c537ca5fd2844c520c0d5bca40d52df49ad
FarrellIon <104186473+FarrellIon@users.noreply.github.com>
2024-04-27 10:25:39 +0700

    config babel webpack

---
+++ Backend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.ts',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'final.ts',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts$|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-typescript'],
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    target: 'node',
+};
