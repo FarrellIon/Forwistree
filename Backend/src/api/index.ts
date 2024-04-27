6f0319a3730db9f4a57b2e6cd2f2090a0a380e53
FarrellIon <104186473+FarrellIon@users.noreply.github.com>
2024-04-27 11:15:05 +0700

    Revert "change route"
    This reverts commit 7542e62dba8c153649d1b892832d4fdaf7a16607.

--- Backend/src/api/index.ts
+++ Backend/src/api/index.ts
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/src/api/database', databaseRoutes);
+app.use('/api/database', databaseRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
