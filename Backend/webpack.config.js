df4896fe1143dcc880e2fd9f093d0c3690d493df
FarrellIon <104186473+FarrellIon@users.noreply.github.com>
2024-04-27 11:07:34 +0700

    add src folder

--- Backend/webpack.config.js
+++ Backend/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './api/index.ts',
+    entry: './src/api/index.ts',
     output: {
         path: path.join(__dirname, 'dist'),
         publicPath: '/',
